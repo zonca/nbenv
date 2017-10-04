8652b8e3e16dd37d4d3e9b05e41a4437c50ea212
Andrea Zonca <code@andreazonca.com>
2017-10-04 11:18:13 -0700

    test with diff

--- tests/test_nbenv.py
+++ tests/test_nbenv.py
@@ -15,16 +15,18 @@ TEST_ENV_INPUT_NAME = "nbenv_test_input_env"
 @pytest.fixture(scope="module")
 def setup_conda_environment():
     conda_package_list = os.path.join(os.path.dirname(__file__),
-                                      "test_conda_list.txt")
-    subprocess.run(["conda", "env", "create", "--name", TEST_ENV_INPUT_NAME,
+                                      "env.yml")
+    subprocess.run(["conda", "env", "create", "--force", "--name", TEST_ENV_INPUT_NAME,
                     "--file", conda_package_list], check=True)
+
 
 def test_fixture(setup_conda_environment):
     """Check that the conda environment was created successfully"""
-    test_environment = \
-        subprocess.check_output(["conda", "env", "export", "--name", TEST_ENV_INPUT_NAME])
-    #TODO compare outputs
-
+    subprocess.run(
+        "conda list --export --name {env} > temp_conda_list.txt".format(env=TEST_ENV_INPUT_NAME),
+        shell=True, check=True)
+    subprocess.run(["diff", "-I", "^#", "temp_conda_list.txt",
+        os.path.join(os.path.dirname(__file__), "test_conda_list.txt")], check=True)
 
 def test_command_line_interface():
     """Test the CLI."""
